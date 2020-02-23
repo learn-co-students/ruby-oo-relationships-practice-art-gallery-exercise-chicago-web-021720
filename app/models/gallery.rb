624e75cce9f76be222e786092549a3f08b41a45e
mpottebaum <mpottebaum@gmail.com>
2020-02-22 18:48:55 -0600

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -7,4 +7,19 @@ class Gallery
     @city = city
   end
 
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    self.paintings.collect {|painting| painting.artist}.uniq
+  end
+
+  def artist_names
+    self.artists.collect {|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    self.paintings.max {|a, b| a.price <=> b.price}
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,6 +2,19 @@ require_relative '../config/environment.rb'
 
 
 
+bob = Artist.new("Bob", 5)
+gwen = Artist.new("Gwen", 7)
+jackie = Artist.new("Jackie", 2)
+
+tate = Gallery.new("Tate", "London")
+whitney = Gallery.new("Whitney", "NYC")
+
+bob.create_painting("Butts", 10000).gallery = tate
+gwen.create_painting("Heyo", 30000).gallery = tate
+
+bob.create_painting("Backs", 100000).gallery = whitney
+gwen.create_painting("Heyo 2", 350000).gallery = whitney
+jackie.create_painting("My First Painting", 40000).gallery = whitney
 
 binding.pry
 
